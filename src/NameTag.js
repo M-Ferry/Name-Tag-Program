657388f724e785b425b00af196f1009bea79283b
M-Ferry <peggy_ferry@yahoo.com>
2025-02-24 21:11:49 -0500

    Third commit added props

--- src/NameTag.js
+++ src/NameTag.js
@@ -1,11 +1,11 @@
 // import the react library to write JSX
 import React from "react";
 
-const NameTag = () => (
+const NameTag = (props) => (
   <div className="name-tag">
     <h3 className="title">Hello</h3>
     <p className="subtitle">My name is</p>
-    <h2 className="name">Margaret</h2>
+    <h2 className="name">{props.name}</h2>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import "./styles.css";
 const App = () => (
   <div className="App">
     <h1>Name Tag Generator</h1>
-    <NameTag />
+    <NameTag name={"Margaret"} />
+    <NameTag name={"Carrie"} />
+    <NameTag name={"Joe"} />
+    <NameTag name={"James"} />
   </div>
 );
 
